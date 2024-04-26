f752952f2216dd9fd47c0dd124cef2d4bb74868a
Kritjomza <132203617+Kritjomza@users.noreply.github.com>
2024-04-26 12:30:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var password = "";
+var expectedPassword = "071245"; // รหัสผ่านที่คาดหวัง
+
+function addDigit(digit) {
+    password += digit.toString();
+    displayPassword();
+    animatePasswordDisplay();
+    updateDigitCount();
+    checkPassword();
+}
+
+function displayPassword() {
+    document.getElementById("passwordDisplay").textContent = password;
+}
+
+function animatePasswordDisplay() {
+    document.getElementById("passwordDisplay").classList.add("animated");
+}
+
+function checkPassword() {
+    if (password.length === expectedPassword.length) {
+        if (password === expectedPassword) {
+            showMessage("รหัสถูก..เย้ะะ🥳");
+            window.location.href = "success.html";
+        } else {
+            showMessage("รหัสผิดจ้าาา😜");
+        }
+        resetPassword();
+    }
+}
+
+function showMessage(message) {
+    document.getElementById("message").textContent = message;
+}
+
+function resetPassword() {
+    password = "";
+    displayPassword();
+    updateDigitCount();
+}
+
+function clearInput() {
+    resetPassword();
+}
+
+function updateDigitCount() {
+    document.getElementById("digitCount").textContent = "จำนวนตัวเลข: " + password.length;
+}
